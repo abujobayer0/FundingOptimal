95dae55b59f315319a78460afe1770ed0fcf331e
abu jobayer <zubayer.munna.dev@gmail.com>
2025-07-10 17:36:10 +0600

    Refactor import statement in AffiliatePartnerFAQs component to use framer-motion

--- src/app/(HomeLayout)/_components/module/becomeAPartner/AffiliatePartnerFAQs.tsx
+++ src/app/(HomeLayout)/_components/module/becomeAPartner/AffiliatePartnerFAQs.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 import FAQAccordion from './FAQAccordion';
 import { useRouter } from 'next/navigation';
 
